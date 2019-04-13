c3b2b77c1f3c670041d5d9b58a0adaa149b890db
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-04-12 23:49:41 -0400

    minified

--- public/js/modules/lightmode.js
+++ public/js/modules/lightmode.js
@@ -1,9 +1,9 @@
-
+//lightmode
 function addLightMode(){
     var body = document.body.classList.add("lightmode");
 }
 
-
+//removing lightmode
 function removeLightMode(){
     var body = document.body.classList.remove("lightmode");
 }
